1a9677c8d1e4d948c77e9f7bf6dd32dc96e86231
geoboom <leegeorgie52@gmail.com>
2020-09-24 00:20:37 +0800

    Wrapped execution of redis multi in promise

--- src/util/redis.js
+++ src/util/redis.js
@@ -10,7 +10,6 @@ client.on("error", (error) => {
 const wrappedRedis = client;
 
 const ops = [
-  "exec", // idk if this will work
   "get",
   "mget",
   "set",
@@ -27,17 +26,24 @@ ops.forEach((op) => {
   wrappedRedis[op] = promisify(client[op]).bind(client);
 });
 
+wrappedRedis.executeMulti = (redisMulti) =>
+  new Promise((resolve, reject) => {
+    redisMulti.exec((err, replies) => {
+      if (err) return reject(err);
+      return resolve(replies);
+    });
+  });
+
 export default wrappedRedis;
 
-const testFn = async () => {
-  const testMulti = wrappedRedis.multi();
-  testMulti.get("1");
-  testMulti.get("2");
-  testMulti.get("3");
-  testMulti.exec((err, replies) => {
-    console.log("err", err);
-    console.log("replies", replies);
-  });
-};
+// for testing, pls ignore
+// const testFn = async () => {
+//   const testMulti = wrappedRedis.multi();
+//   testMulti.hgetall("game-539144");
+//   testMulti.hgetall("user-abc123");
+//   testMulti.hgetall("user-testing1");
+//   const res = await executeMulti(testMulti);
+//   console.log(res);
+// };
 
-testFn();
+// testFn();
