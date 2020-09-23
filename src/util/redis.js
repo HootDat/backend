41c51bb049e1ca6f053e71f4ecb891e90e2e50d9
geoboom <leegeorgie52@gmail.com>
2020-09-23 22:53:37 +0800

    Redis multi() promisifying WIP

--- src/util/redis.js
+++ src/util/redis.js
@@ -7,10 +7,12 @@ client.on("error", (error) => {
   console.error("redis error", error);
 });
 
-const wrappedRedis = redis;
+const wrappedRedis = client;
 
 const ops = [
+  "exec", // idk if this will work
   "get",
+  "mget",
   "set",
   "hset",
   "hget",
@@ -26,3 +28,16 @@ ops.forEach((op) => {
 });
 
 export default wrappedRedis;
+
+const testFn = async () => {
+  const testMulti = wrappedRedis.multi();
+  testMulti.get("1");
+  testMulti.get("2");
+  testMulti.get("3");
+  testMulti.exec((err, replies) => {
+    console.log("err", err);
+    console.log("replies", replies);
+  });
+};
+
+testFn();
