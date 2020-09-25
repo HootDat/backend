fc1cef50d588e3bde7e107fe24348cd3a0877747
geoboom <leegeorgie52@gmail.com>
2020-09-25 22:11:32 +0800

    DB migration script in procfile

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 # heroku config:set TYPEORM_CONNECTION=postgres -a api-hoot-dat
-# heroku config:set TYPEORM_HOST=postgres -a api-hoot-dat
-# heroku config:set TYPEORM_USERNAME=postgres -a api-hoot-dat
-# heroku config:set TYPEORM_PASSWORD=hootdatbest -a api-hoot-dat
-# heroku config:set TYPEORM_DATABASE=hootdat -a api-hoot-dat
+heroku config:set TYPEORM_HOST=ec2-52-213-173-172.eu-west-1.compute.amazonaws.com -a api-hoot-dat
+heroku config:set TYPEORM_USERNAME=houopfwhkowlnc -a api-hoot-dat
+heroku config:set TYPEORM_PASSWORD=0758c60b4b51f1e6fa737c89fe2db2d81ddbcbeee98bee98434c5bc577b17197 -a api-hoot-dat
+heroku config:set TYPEORM_DATABASE=d40ujtgpqbcp8v -a api-hoot-dat
 # heroku config:set TYPEORM_PORT=5432 -a api-hoot-dat
 heroku config:set TYPEORM_SYNCHRONIZE=false -a api-hoot-dat
 # heroku config:set TYPEORM_LOGGING=true -a api-hoot-dat
@@ -11,3 +11,5 @@ heroku config:set TYPEORM_SYNCHRONIZE=false -a api-hoot-dat
 # heroku config:set FACEBOOK_ID=754220301289665 -a api-hoot-dat
 # heroku config:set FACEBOOK_SECRET=41860e58c256a3d7ad8267d3c1939a4a -a api-hoot-dat
 # heroku config:set POSTGRES_DB=hootdat POSTGRES_USER=postgres POSTGRES_PASSWORD=hootdatbest -a api-hoot-dat
+
+# postgres://houopfwhkowlnc:0758c60b4b51f1e6fa737c89fe2db2d81ddbcbeee98bee98434c5bc577b17197@ec2-52-213-173-172.eu-west-1.compute.amazonaws.com:5432/d40ujtgpqbcp8v
